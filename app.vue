bf342022a0c620e8310abd7f3d5aa8fec1a2f0e6
Paul Pestov <pestov@sub.uni-goettingen.de>
2023-06-02 14:19:54 +0200

    Add nuxt

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+const { data } = await useFetch(
+  () => `https://api.gs.sub.uni-goettingen.de/v1/monasteries/list?limit=30&offset=0`
+)
+
+</script>
+
+<template>
+  <div v-for="item in data.list">
+    {{item.name}}
+  </div>
+</template>
