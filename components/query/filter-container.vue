a2ffbfa56702c00812ebb17bfa6b31fd2f6bfd1a
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2023-07-21 00:08:16 +0200

    fix: warnings at components

--- components/query/filter-container.vue
+++ components/query/filter-container.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 const props = defineProps<{
-  title: String
+  title?: String
 }>();
 
 </script>
 <template>
   <div class="flex flex-col mb-6">
-    <h3 class="mb-2">{{ title }}</h3>
+    <h3 v-if="title" class="mb-2">{{ title }}</h3>
     <div class="flex">
       <slot/>
     </div>
