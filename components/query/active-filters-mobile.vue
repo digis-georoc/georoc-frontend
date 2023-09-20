bcdf48db9ca49c3daba5954af050e340b8b4235d
Paul Pestov <pestov@sub.uni-goettingen.de>
2023-09-20 11:08:46 +0200

    feat: switch positions of filters and list button on mobile

--- components/query/active-filters-mobile.vue
+++ components/query/active-filters-mobile.vue
@@ -6,18 +6,18 @@ const emit = defineEmits(['is-filter-open', 'is-list-open'])
   <div class="dark:bg-zinc-800 w-full px-4 py-2">
     <div class="flex">
       <BaseButton
-        :text="$t('list_view')"
-        icon="ic:round-format-list-bulleted"
-        display="flat"
-        class="flex xl:hidden me-1"
-        @click="emit('list', true)"
-      />
-      <BaseButton
         :text="$t('filters')"
         icon="mi:filter"
         display="flat"
-        class="flex xl:hidden ms-auto"
+        class="flex xl:hidden"
         @click="emit('filter', true)"
+      />
+      <BaseButton
+        :text="$t('list_view')"
+        icon="ic:round-format-list-bulleted"
+        display="flat"
+        class="flex xl:hidden ms-auto"
+        @click="emit('list', true)"
       />
     </div>
   </div>
