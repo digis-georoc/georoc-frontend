d639c5886d28219bf49b121db3d1e921bea5a6ae
Paul Pestov <pestov@sub.uni-goettingen.de>
2023-10-09 10:18:05 +0200

    fix: chemistry input widths

--- components/query/filter/chemistry/select.vue
+++ components/query/filter/chemistry/select.vue
@@ -16,8 +16,8 @@ const emit = defineEmits<{
   <BaseSelect :options="elementTypesOptions" :model-value="modelValue.type" @update:modelValue="modelValue.type = $event" :title="$t('element_type')" class="mb-4" />
   <BaseSelect :options="elementOptions" :model-value="modelValue.element" @update:modelValue="modelValue.element = $event" :title="$t('element')" class="mb-4" />
   <div class="flex space-x-2">
-    <BaseInput title="Min" type="number" :model-value="modelValue.min" @update:modelValue="modelValue.min = $event" />
-    <BaseInput title="Max" type="number" :model-value="modelValue.max" @update:modelValue="modelValue.max = $event" />
+    <BaseInput class="w-1/2" title="Min" type="number" :model-value="modelValue.min" @update:modelValue="modelValue.min = $event" />
+    <BaseInput class="w-1/2" title="Max" type="number" :model-value="modelValue.max" @update:modelValue="modelValue.max = $event" />
   </div>
 
 </template>
