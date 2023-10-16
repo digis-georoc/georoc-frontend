19afa85d7db504d1b1ca42350c5fd792b640ecba
Paul Pestov <pestov@sub.uni-goettingen.de>
2023-10-16 16:19:11 +0200

    fix: update primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#754672',
-        'primary-50' : '#f0dfee',
-        'primary-100' : '#dbc3d9',
-        'primary-300' : '#a374a2',
-        'primary-700' : '#5e385c'
+        primary: '#7a495d',
+        'primary-50' : '#f0e1e7',
+        'primary-100' : '#dbcad1',
+        'primary-300' : '#a3818f',
+        'primary-700' : '#5e3b4a'
       }
     }
   }
