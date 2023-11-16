4d1f0f6f9f2ad4b0c21aa0e1d86431d6c64c48b8
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2023-11-17 00:05:37 +0100

    refactor: use PrimeVue in radio group comp

--- components/base/radio-group.vue
+++ components/base/radio-group.vue
@@ -1,89 +1,67 @@
 <script setup lang="ts">
-import {
-  RadioGroup,
-  RadioGroupLabel,
-  RadioGroupOption,
-} from '@headlessui/vue'
+import SelectButton from 'primevue/selectbutton'
 
 const props = defineProps<{
   modelValue: RadioGroupOption
   options: RadioGroupOption[]
 }>()
 
-// watch(() => props.options, (value) => {
-//   selected.value = value[0]
-// }, { immediate: true })
-// const selected = ref<Option>()
-
 const emit = defineEmits(['change', 'update:modelValue'])
+
+const selectedOptionValue = ref(props.modelValue.value)
+function onUpdate(option: RadioGroupOption) {
+  selectedOptionValue.value = option.value
+  emit('update:modelValue', option)
+}
 
 </script>
 <template>
   <div class="w-full px-4 py-16">
     <div class="mx-auto w-full max-w-md">
-      <RadioGroup :model-value="modelValue" @update:modelValue="emit('update:modelValue', $event)">
-        <RadioGroupLabel class="sr-only">Server size</RadioGroupLabel>
-        <div class="space-y-2">
-          <RadioGroupOption
-              as="template"
-              v-for="option in options"
-              :key="option.value"
-              :value="option"
-              v-slot="{ active, checked }"
-          >
-            <div
-                :class="[
-                active
-                  ? 'ring-2 ring-primary dark:ring-primary-300 dark:ring-opacity-50 ring-opacity-25'
-                  : '',
-                checked ? 'bg-primary text-white ' : 'bg-white  ',
-              ]"
-                class="relative flex cursor-pointer rounded-lg px-5 py-4 focus:outline-none border-2"
-            >
-              <div class="flex w-full items-center justify-between">
-                <div class="flex items-center">
-                  <div class="text-sm">
-                    <RadioGroupLabel
-                        as="p"
-                        :class="checked ? 'text-white' : 'text-gray-900'"
-                        class="font-medium"
-                    >
-                      {{ option.label }}
-                    </RadioGroupLabel>
-<!--                    <RadioGroupDescription-->
-<!--                        as="span"-->
-<!--                        :class="checked ? 'text-sky-100' : 'text-gray-500'"-->
-<!--                        class="inline"-->
-<!--                    >-->
-<!--                      <span> {{ plan.ram }}/{{ plan.cpus }}</span>-->
-<!--                      <span aria-hidden="true"> &middot; </span>-->
-<!--                      <span>{{ plan.disk }}</span>-->
-<!--                    </RadioGroupDescription>-->
-                  </div>
-                </div>
-                <div v-show="checked" class="shrink-0 text-white">
-                  <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none">
-                    <circle
-                        cx="12"
-                        cy="12"
-                        r="12"
-                        fill="#fff"
-                        fill-opacity="0.2"
-                    />
-                    <path
-                        d="M7 13l3 3 7-7"
-                        stroke="#fff"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </RadioGroupOption>
-        </div>
-      </RadioGroup>
+      <SelectButton
+        :options="options"
+        option-label="label"
+        :model-value="modelValue" @update:modelValue="onUpdate"
+        unstyled
+        :pt="{
+          root: {
+            class: 'flex'
+          },
+          button: ({ context }) => ({
+            class: [
+              context.active ? 'bg-primary text-white z-10' : '',
+              options.findIndex(option => option.value === context.option.value) === 0 ? 'rounded-l-lg border-l border-r' : '',
+              {
+                'border-r':
+                  options.length > 3 &&
+                  !(options.findIndex(option => option.value === context.option.value) === 0) &&
+                  !(options.findIndex(option => option.value === context.option.value) === options.length - 1)
+              },
+              options.findIndex(option => option.value === context.option.value) === options.length - 1 ? 'rounded-r-lg border-r border-l' : '',
+              // { 'ring-2 ring-offset-2 ring-primary-300': context.focused },
+              { 'hover:bg-zinc-100': !context.active },
+              'relative py-2 px-4 flex-1 flex items-center font-semibold border-t border-b cursor-pointer transition-colors outline-none focus:ring-2 focus:ring-primary-300'
+            ]
+          }),
+        }"
+      >
+        <template v-slot:option="{ option }: { option: RadioGroupOption }">
+          <span>{{ option.label }}</span>
+          <Transition>
+            <Icon v-if="selectedOptionValue === option.value" name="ic:baseline-check-circle" class="ml-2 text-primary-300 text-lg"/>
+          </Transition>
+        </template>
+      </SelectButton>
     </div>
   </div>
 </template>
+<style scoped>
+.v-enter-active {
+  transition: opacity 0.4s 0.1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
