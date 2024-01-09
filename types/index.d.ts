c8a09c0b29a1a45686388f85bfd85d423913042d
Jasper Frerichs <jasper.frerichs@sub.uni-goettingen.de>
2024-01-09 13:53:44 +0000

    feat: add error handling for precompiled files page and expert dataset page

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,40 +1,40 @@
-import {Feature, Point, Polygon} from "geojson";
-import type { TreeNode } from "primevue/tree";
+import { Feature, Point, Polygon } from 'geojson'
+import type { TreeNode } from 'primevue/tree'
 
 declare global {
   interface QueryFilter {
-    name: FilterKey,
+    name: FilterKey
     value: any
   }
 
   interface QueryListItem {
-    sampleID: number,
-    samplename: string,
-    latitude: number,
+    sampleID: number
+    samplename: string
+    latitude: number
     longitude: number
   }
 
   interface QueryLocationFeature {
-    coordinates: string[],
+    coordinates: string[]
     type: string
   }
 
   interface QueryLocationsResponse {
-    bbox: Feature<Polygon>,
+    bbox: Feature<Polygon>
     clusters: QueryLocationsCluster[]
   }
 
   interface QueryLocationsCluster {
-    centroid: Feature<Point>,
+    centroid: Feature<Point>
     convexHull: Feature
   }
 
   interface QueryListResponse {
-    data: QueryListItem[],
-    numItems: number,
-    totalCount: number,
+    data: QueryListItem[]
+    numItems: number
+    totalCount: number
     error?: {
-      statusCode: number,
+      statusCode: number
       statusMessage: string
     }
   }
@@ -45,45 +45,45 @@ declare global {
 
   interface QueryState {
     filters: {
-      active: QueryFilter[],
-      material: QueryFilter | null,
-      bbox: QueryFilter | null,
-      WRGL: QueryFilter[],
-      INC: QueryFilter[],
+      active: QueryFilter[]
+      material: QueryFilter | null
+      bbox: QueryFilter | null
+      WRGL: QueryFilter[]
+      INC: QueryFilter[]
       MIN: QueryFilter[]
-    },
-    result: QueryLocationsResponse | null,
+    }
+    result: QueryLocationsResponse | null
     listResult: QueryListResponse | null
   }
 
   interface RocktypesResponseItem {
-    label: string,
+    label: string
     value: string
   }
 
   interface MineralsResponseItem {
-    label: string,
+    label: string
     value: string
   }
 
   interface PolygonState {
-    activeState: number,
+    activeState: number
   }
 
   interface MultiselectOption {
-    value: string,
-    label: string,
+    value: string
+    label: string
     active: boolean
   }
 
   interface RadioGroupOption {
-    value: string,
-    label: string,
+    value: string
+    label: string
   }
 
   interface ChemicalElement {
-    value: string,
-    label: string,
+    value: string
+    label: string
     unit: string
   }
 
@@ -91,33 +91,32 @@ declare global {
     name: string
   }
 
-
   interface SelectedChemicalElement {
-    type: RadioGroupOption | null,
-    element: RadioGroupOption | null,
-    min: number,
+    type: RadioGroupOption | null
+    element: RadioGroupOption | null
+    min: number
     max: number
   }
 
   interface RockClass {
-    value: string,
+    value: string
     label: string
   }
 
   interface HostMaterial {
-    count: number,
-    label: string,
-    value: string,
+    count: number
+    label: string
+    value: string
   }
 
   interface InclusionMaterial {
-    count: number,
-    label: string,
-    value: string,
+    count: number
+    label: string
+    value: string
   }
 
   interface BaseResponse<T> {
-    data: T[],
+    data: T[]
     numItems: number
   }
 
@@ -130,21 +129,26 @@ declare global {
   type InclusionMaterialsResponse = BaseResponse<InclusionMaterial>
 
   interface ExpertDatasetRow {
-    authors: string,
-    title: string,
-    versionNr: string,
-    productionDate: number,
-    persistentUrl: string,
+    authors: string
+    title: string
+    versionNr: string
+    productionDate: number
+    persistentUrl: string
   }
 
   interface ExpertDatasetResponse {
     datasets: ExpertDatasetRow[]
   }
 
+  interface DataverseOKResponse {
+    status: 'OK'
+    data: any
+  }
+
   interface PrecompiledFilePreview {
-    protocol: string,
-    authority: string,
-    identifier: string,
+    protocol: string
+    authority: string
+    identifier: string
     title: string
   }
 
@@ -153,23 +157,23 @@ declare global {
   }
 
   interface PrecompiledFiles {
-    title: string,
-    authors: string[],
-    persistentUrl: string,
-    description: string,
-    publicationDate: Date,
-    subject: string,
-    datasetPersistentId: string,
-    productionDate: Date,
+    title: string
+    authors: string[]
+    persistentUrl: string
+    description: string
+    publicationDate: Date
+    subject: string
+    datasetPersistentId: string
+    productionDate: Date
     license: {
-      name: string,
+      name: string
       uri: string
     }
     files: {
-      contentType: string,
-      label: string,
-      persistentId: string,
-      filesize: number,
+      contentType: string
+      label: string
+      persistentId: string
+      filesize: number
       persistentUrl: string
     }[]
   }
@@ -179,17 +183,15 @@ declare global {
   }
 
   interface SelectedRockType {
-    label: string,
-    value: string,
+    label: string
+    value: string
     classes: RadioGroupOption[]
   }
 
   interface ChemistryState {
-    nodes: TreeNode[],
+    nodes: TreeNode[]
     selected: TreeNode[]
   }
 }
 
 export {}
-
-
--- utils/api.ts
+++ utils/api.ts
@@ -1,9 +1,9 @@
-async function getSamples(params: URLSearchParams, { signal }: AbortController): Promise<QueryLocationsResponse | null> {
+async function getSamples(
+  params: URLSearchParams,
+  { signal }: AbortController,
+): Promise<QueryLocationsResponse | null> {
   try {
-    const res: Response = await fetch(
-      `/api/query?${params}`,
-      { signal }
-    )
+    const res: Response = await fetch(`/api/query?${params}`, { signal })
     return await res.json()
   } catch (err) {
     if (err.name === 'AbortError') throw new Error('abort')
@@ -13,71 +13,122 @@ async function getSamples(params: URLSearchParams, { signal }: AbortController):
 }
 
 async function getRocktypes(): Promise<RocktypesResponse | null> {
-  const { data} = await useFetch<RocktypesResponse>('/api/rocktypes',)
+  const { data } = await useFetch<RocktypesResponse>('/api/rocktypes')
   return data.value
 }
 
 async function getMinerals(): Promise<MineralsResponse | null> {
-  const { data} = await useFetch<MineralsResponse>(
-    '/api/minerals',
-  )
+  const { data } = await useFetch<MineralsResponse>('/api/minerals')
   return data.value
 }
 
-async function getList(params: URLSearchParams): Promise<QueryListResponse | null> {
+async function getList(
+  params: URLSearchParams,
+): Promise<QueryListResponse | null> {
   const res: Response = await fetch(`/api/list?${params}`)
 
   if (res.ok) {
     return await res.json()
   } else {
-   throw { statusCode: res.status, statusMessage: res.statusText }
+    throw { statusCode: res.status, statusMessage: res.statusText }
   }
 }
 
-async function getExpertDatasets() {
-    const { data } = await useFetch<ExpertDatasetResponse>('/api/expertdatasets')
-    return data.value
-}
-
-async function getPrecompiledPreview() {
-    const { data } = await useFetch<PrecompiledFilePreviews>('/api/precompiled-files/preview')
-    return data.value
-}
-
-async function getPrecompiledFiles(protocol: string, authority: string, id: string) {
-  const params = new URLSearchParams({protocol, authority, id});
-  const { data } = await useFetch<PrecompiledFiles>('/api/precompiled-files/files' + (params ? `?${params.toString()}` : ''));
+async function getExpertDatasets(): Promise<ExpertDatasetResponse | null> {
+  const { error, data } = await useFetch<ExpertDatasetResponse>(
+    '/api/expertdatasets',
+  )
+  if (error.value) {
+    throw createError({
+      statusCode: error.value.statusCode,
+      statusMessage: error.value.statusMessage,
+    })
+  }
   return data.value
 }
 
-async function getPrecompiledFile(identifier: string, isDataset: boolean) {
-  const params = new URLSearchParams({identifier, isDataset: isDataset.toString()});
-  const { data }  = await useFetch<Blob>('/api/precompiled-files/download-file' + (params ? `?${params.toString()}` : ''));
+async function getPrecompiledPreview(): Promise<PrecompiledFilePreviews | null> {
+  const { error, data } = await useFetch<PrecompiledFilePreviews>(
+    '/api/precompiled-files/preview',
+  )
+  if (error.value) {
+    throw createError({
+      statusCode: error.value.statusCode,
+      statusMessage: error.value.statusMessage,
+    })
+  }
+  return data.value
+}
+
+async function getPrecompiledFiles(
+  protocol: string,
+  authority: string,
+  id: string,
+): Promise<PrecompiledFiles | null> {
+  const params = new URLSearchParams({ protocol, authority, id })
+  const { error, data } = await useFetch<PrecompiledFiles>(
+    '/api/precompiled-files/files' + (params ? `?${params.toString()}` : ''),
+  )
+  if (error.value) {
+    throw createError({
+      statusCode: error.value.statusCode,
+      statusMessage: error.value.statusMessage,
+    })
+  }
+  return data.value
+}
+
+async function getPrecompiledFile(
+  identifier: string,
+  isDataset: boolean,
+): Promise<Blob | null> {
+  const params = new URLSearchParams({
+    identifier,
+    isDataset: isDataset.toString(),
+  })
+  const { error, data } = await useFetch<Blob>(
+    '/api/precompiled-files/download-file' +
+      (params ? `?${params.toString()}` : ''),
+  )
+  if (error.value) {
+    throw createError({
+      statusCode: error.value.statusCode,
+      statusMessage: error.value.statusMessage,
+    })
+  }
   return data.value
 }
 
 async function getElements(type: string): Promise<ElementsResponse | null> {
-  const { data} = await useFetch<ElementsResponse>('/api/elements' + (type ? `?type=${type}` : ''))
+  const { data } = await useFetch<ElementsResponse>(
+    '/api/elements' + (type ? `?type=${type}` : ''),
+  )
   return data.value
 }
 
 async function getElementTypes(): Promise<ElementsResponse | null> {
-  const { data} = await useFetch<ElementsResponse>('/api/element-types')
+  const { data } = await useFetch<ElementsResponse>('/api/element-types')
   return data.value
 }
 
-async function getRockClasses(rockType: string): Promise<RockClassResponse | null> {
-  const { data} = await useFetch<RockClassResponse>('/api/rock-classes' + (rockType ? `?rocktype=${rockType}` : ''))
+async function getRockClasses(
+  rockType: string,
+): Promise<RockClassResponse | null> {
+  const { data } = await useFetch<RockClassResponse>(
+    '/api/rock-classes' + (rockType ? `?rocktype=${rockType}` : ''),
+  )
   return data.value
 }
 
 async function getHostMaterials(): Promise<HostMaterialsResponse | null> {
-  const { data} = await useFetch<HostMaterialsResponse>('/api/host-materials')
+  const { data } = await useFetch<HostMaterialsResponse>('/api/host-materials')
   return data.value
 }
 
 async function getInclusionMaterials(): Promise<HostMaterialsResponse | null> {
-  const { data} = await useFetch<HostMaterialsResponse>('/api/inclusion-materials')
+  const { data } = await useFetch<HostMaterialsResponse>(
+    '/api/inclusion-materials',
+  )
   return data.value
 }
 
@@ -94,6 +145,5 @@ export {
   getInclusionMaterials,
   getPrecompiledFiles,
   getPrecompiledFile,
-  getPrecompiledPreview
+  getPrecompiledPreview,
 }
-
