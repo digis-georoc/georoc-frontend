146d2929f01d7b2df82918b7182814f28738bfd5
Paul Pestov <pestov@sub.uni-goettingen.de>
2024-05-07 15:17:58 +0200

    fix: collapse toggle in card header

--- components/base/card.vue
+++ components/base/card.vue
@@ -13,17 +13,19 @@ watch(() => props.collapsed, (value) => _collapsed.value = value, { immediate: t
 <template>
   <div class="flex flex-col border dark:border-zinc-600 rounded-md bg-white dark:bg-zinc-800 overflow-hidden">
     <div
-      class="flex relative pl-4 pr-12 py-3 border-b transition-colors hover:cursor-pointer"
+      class="flex relative pr-12 border-b transition-colors"
       :class="{'dark:border-zinc-600': !_collapsed, 'border-transparent delay-300': _collapsed }"
-      @click="_collapsed = !_collapsed"
     >
-      <h3 class="font-bold">{{ title }}</h3>
-      <slot name="header-right" />
+      <h3 class="font-bold flex-1 py-3 pl-4 hover:cursor-pointer" @click="_collapsed = !_collapsed">{{ title }}</h3>
+      <div class="py-3 flex flex-shrink-0">
+        <slot name="header-right" />
+      </div>
       <BaseButton
         v-if="!noCollapse"
         icon="carbon:chevron-down"
         class="absolute right-2 top-1/2 -translate-y-1/2"
         display="flat"
+        @click="_collapsed = !_collapsed"
       ></BaseButton>
     </div>
     <div class="flex flex-col overflow-hidden transition-all duration-600" :class="{
