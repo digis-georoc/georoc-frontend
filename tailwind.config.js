f41607a62638ffd4bf5a574a50a56575fa0e9692
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2024-10-17 13:17:14 +0200

    feat: set less blueish dark mode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,27 +5,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        // primary: '#399ed4',
-        // 'primary-50' : '#dbf1ff',
-        // 'primary-100' : '#97cae6',
-        // 'primary-300' : '#4eb0e6',
-        // 'primary-400' : '#50A5D3',
-        // 'primary-700' : '#3a7899',
-
-        primary: '#0668a1', // #0A66C2
+        primary: '#0668a1',
         'primary-50' : '#e6f7ff',
         'primary-100' : '#c3d8e6',
         'primary-300' : '#067fc9',
         'primary-400' : '#005f9b',
         'primary-700' : '#004f80',
-
-        // primary: '#265cbf',
-        // 'primary-50' : '#e3ecfc',
-        // 'primary-100' : '#a7b8d6',
-        // 'primary-300' : '#2d6de3',
-        // 'primary-400' : '#265cbf',
-        // 'primary-700' : '#1d3e7d',
-        gray: colors.slate,
+        gray: {
+          '50': colors.slate['50'],
+          '100': colors.slate['100'],
+          '200': colors.slate['200'],
+          '300': colors.slate['300'],
+          '400': colors.slate['400'],
+          '500': colors.slate['500'],
+          '600': colors.zinc['600'],
+          '700': colors.zinc['700'],
+          '800': colors.zinc['800'],
+          '900': colors.zinc['900'],
+          '950': colors.zinc['950'],
+        },
       },
       screens: {
         'landscape': {
