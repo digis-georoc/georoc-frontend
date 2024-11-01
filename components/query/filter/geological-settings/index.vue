49f9ed1f94c2730390da524903bdf21be7f3bcac
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2024-11-01 16:02:35 +0100

    feat: add old geological settings list

--- components/query/filter/geological-settings/index.vue
+++ components/query/filter/geological-settings/index.vue
@@ -7,118 +7,26 @@ const queryStore = useQueryStore()
 const selectedTemp = ref<TreeNode[]>(<TreeNode[]>[])
 const selected = ref<TreeNode[]>(<TreeNode[]>[])
 
-const options = [
-  {
-    "label": "Oceanic crust",
-    "altLabel": "ocean floor, ocean basin, old oceanic spreading center, abyssal hill, fossil ridge, failed rift",
-    "key": "OC"
-  },
-  {
-    "label": "Oceanic spreading center",
-    "altLabel": "mid oceanic ridges, on- and off-axis spreading centres, triple junction",
-    "key": "OSC"
-  },
-  {
-    "label": "Oceanic fracture zone",
-    "altLabel": "oceanic fracture zone, transform fault",
-    "key": "OFZ"
-  },
-  {
-    "label": "Oceanic island",
-    "altLabel": "hot spot track, oceanic hot spot, petite spot",
-    "key": "OI"
-  },
-  {
-    "label": "Seamount",
-    "altLabel": "seamount",
-    "key": "SM"
-  },
-  {
-    "label": "Aseismic ridge",
-    "altLabel": "aseismic ridge, submarine ridge",
-    "key": "AR"
-  },
-  {
-    "label": "Large igneous province (Oceanic)",
-    "altLabel": "ocean basin flood basalt, oceanic plateau",
-    "key": "LIP-O"
-  },
-  {
-    "label": "Continental intraplate setting",
-    "altLabel": "intraplate volcanics, continental volcanic field, plutonic bodies",
-    "key": "CIS"
-  },
-  {
-    "label": "Large igneous province (Continental)",
-    "altLabel": "continental flood basalt, large igneous province",
-    "key": "LIP-C"
-  },
-  {
-    "label": "Continental rift",
-    "altLabel": "continental rift, continental graben zone, incipient rift, rift valley",
-    "key": "CR"
-  },
-  {
-    "label": "Continental collision zone",
-    "altLabel": "orogenic volcanic and plutonic rocks",
-    "key": "CCZ"
-  },
-  {
-    "label": "Subduction zone (Ocean-Ocean)",
-    "altLabel": "oceanic subduction zones, volcanic arc, trench",
-    "key": "SZ-OO"
-  },
-  {
-    "label": "Subduction zone (Ocean-Continent)",
-    "altLabel": "active continental margin, continental arc, trench",
-    "key": "SZ-OC"
-  },
-  {
-    "label": "Back arc (Oceanic)",
-    "altLabel": "oceanic back arc basin",
-    "key": "BA-O"
-  },
-  {
-    "label": "Back arc (Continental)",
-    "altLabel": "continental back arc basin",
-    "key": "BA-C"
-  },
-  {
-    "label": "Forearc (Oceanic)",
-    "altLabel": "oceanic forearc setting",
-    "key": "FA-O"
-  },
-  {
-    "label": "Ophiolite",
-    "altLabel": "ophiolite complex",
-    "key": "OP"
-  },
-  {
-    "label": "Archean greengray belt",
-    "altLabel": "-",
-    "key": "AGB"
-  },
-  {
-    "label": "Archean TTG/gneiss complex",
-    "altLabel": "-",
-    "key": "TTG"
-  },
-  {
-    "label": "Paleo subduction zone",
-    "altLabel": "-",
-    "key": "PSZ"
-  },
-  {
-    "label": "Complex tectonic setting",
-    "altLabel": "-",
-    "key": "CTS"
-  },
-  {
-    "label": "Unknown",
-    "altLabel": "-",
-    "key": "UNK"
-  }
+const oldList = [
+  "COMPLEX VOLCANIC SETTINGS",
+  "OCEANIC PLATEAU",
+  "INTRAPLATE VOLCANICS",
+  "ARCHEAN CRATON (INCLUDING GREENSTONE BELTS)",
+  "CONTINENTAL FLOOD BASALT",
+  "SEAMOUNT",
+  "SUBMARINE RIDGE",
+  "CONVERGENT MARGIN",
+  "OCEAN ISLANDI",
+  "OCEAN-BASIN FLOOD BASALT",
+  "convergent margin",
+  "OCEAN ISLAND",
+  "RIFT VOLCANICS"
 ]
+
+const options = oldList.map(item => ({
+  label: item,
+  key: item
+}));
 
 function submit() {
   selected.value = [...selectedTemp.value]
